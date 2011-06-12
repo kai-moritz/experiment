cfe2f629f5457cc2b72e09cc376b4aaf1b006179
Kai Moritz <kai@coolibri.de>
2011-06-12 13:31:57 +0200

    Fehler in SelectionController auf den Punkt gebracht

--- src/main/java/de/halbekunst/versuch/controller/SelectionController.java
+++ src/main/java/de/halbekunst/versuch/controller/SelectionController.java
@@ -2,15 +2,8 @@ package de.halbekunst.versuch.controller;
 
 import de.halbekunst.versuch.dao.PictureDao;
 import de.halbekunst.versuch.dao.SelectionDao;
-import de.halbekunst.versuch.model.Picture;
 import de.halbekunst.versuch.model.SavedSelection;
-import de.halbekunst.versuch.model.Selection;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Date;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -40,230 +33,69 @@ public class SelectionController {
   @Autowired SelectionDao selectionDao;
 
 
-  @Transactional(readOnly=false)
-  @RequestMapping("/selection/clone.html")
-  ModelAndView clone(HttpServletRequest request, HttpServletResponse response, @RequestParam Long id) {
-    ModelAndView mav = new ModelAndView(VIEW);
-    CookieBackedSelection selection = new CookieBackedSelection(selectionDao.get(id));
-    response.addCookie(selection.toCookie());
-    mav.addObject("selection", selection);
-    return mav;
-  }
-
   @Transactional(readOnly=true)
   @RequestMapping("/selection/show.html")
   ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
-    ModelAndView mav = new ModelAndView(VIEW);
-    mav.addObject("selection", new CookieBackedSelection(request.getCookies()));
-    return mav;
-  }
+    Cookie[] cookies = request.getCookies();
+    SavedSelection selection = null;
+    if (cookies != null) {
+      for (Cookie received : cookies) {
+        if (COOKIE_NAME.equals(received.getName())) {
+          received.setPath(COOKIE_PATH);
+          String value = received.getValue();
+          try {
+            selection = (SavedSelection) selectionDao.get(Long.parseLong(value));
+          }
+          catch (NullPointerException e) {
+            log.info("Die gemerkte Auswahl {} existiert nicht (mehr)!", value);
+          }
+          break;
+        }
+      }
+    }
 
-  @Transactional(readOnly=false)
-  @RequestMapping("/selection/sort.html")
-  ModelAndView sort(HttpServletRequest request, HttpServletResponse response) {
-    CookieBackedSelection selection = new CookieBackedSelection(request.getCookies());
-    Collections.sort(selection.getPictures());
-    response.addCookie(selection.toCookie());
     ModelAndView mav = new ModelAndView(VIEW);
-    mav.addObject("selection", new CookieBackedSelection(request.getCookies()));
+    mav.addObject("selection", selection);
     return mav;
   }
 
   @Transactional(readOnly=false)
   @RequestMapping("/selection/add.html")
   ModelAndView addPicture(HttpServletRequest request, HttpServletResponse response, @RequestParam Long id) {
-    CookieBackedSelection selection = new CookieBackedSelection(request.getCookies());
-    selection.addPicture(id);
-    response.addCookie(selection.toCookie());
-    ModelAndView mav = new ModelAndView(VIEW);
-    mav.addObject("selection", selection);
-    return mav;
-  }
-
-  @Transactional(readOnly=false)
-  @RequestMapping("/selection/remove.html")
-  ModelAndView removePicture(HttpServletRequest request, HttpServletResponse response, @RequestParam Long id) {
-    CookieBackedSelection selection = new CookieBackedSelection(request.getCookies());
-    selection.removePicture(id);
-    response.addCookie(selection.toCookie());
-    ModelAndView mav = new ModelAndView(VIEW);
-    mav.addObject("selection", selection);
-    return mav;
-  }
-
-  @Transactional(readOnly=false)
-  @RequestMapping("/selection/move/up.html")
-  ModelAndView moveUp(HttpServletRequest request, HttpServletResponse response, @RequestParam(required=true) Long id) {
-    CookieBackedSelection selection = new CookieBackedSelection(request.getCookies());
-    List<Picture> pictures = selection.getPictures();
-    Picture picture = pictureDao.get(id);
-    int pos = pictures.indexOf(picture);
-    if (pos+1 < pictures.size()) {
-      pictures.remove(pos);
-      pictures.add(pos+1, picture);
-    }
-    response.addCookie(selection.toCookie());
-    ModelAndView mav = new ModelAndView(VIEW);
-    mav.addObject("selection", selection);
-    return mav;
-  }
-
-  @Transactional(readOnly=false)
-  @RequestMapping("/selection/move/down.html")
-  ModelAndView moveDown(HttpServletRequest request, HttpServletResponse response, @RequestParam(required=true) Long id) {
-    CookieBackedSelection selection = new CookieBackedSelection(request.getCookies());
-    List<Picture> pictures = selection.getPictures();
-    Picture picture = pictureDao.get(id);
-    int pos = pictures.indexOf(picture);
-    if (pos > 0) {
-      pictures.remove(pos);
-      pictures.add(pos-1, picture);
-    }
-    response.addCookie(selection.toCookie());
-    ModelAndView mav = new ModelAndView(VIEW);
-    mav.addObject("selection", selection);
-    return mav;
-  }
-
-
-  class CookieBackedSelection implements Selection, Serializable {
-    private Long id = null;
-    private String name;
-    private List<Picture> pictures;
-    private SavedSelection selection;
-    private Cookie cookie;
-
-
-    CookieBackedSelection(Selection original) {
-      cookie = new Cookie(COOKIE_NAME,"");
-      cookie.setPath(COOKIE_PATH);
-      cookie.setMaxAge(COOKIE_MAX_AGE);
-      name = "Kopie von " + original.toString();
-      if (true) {
-        selection = new SavedSelection();
-        selection.setName(name);
-        selection.getPictures().addAll(original.getPictures());
-        pictures = selection.getPictures();
-      }
-      else {
-        pictures = new ArrayList<Picture>(original.getPictures());
-      }
-    }
-
-    CookieBackedSelection(Cookie[] cookies) {
-      try {
-        if (cookies != null) {
-          for (Cookie received : cookies) {
-            if (COOKIE_NAME.equals(received.getName())) {
-              cookie = received;
-              cookie.setPath(COOKIE_PATH);
-              String value = cookie.getValue();
-              if (false) {
-                name = "Gemerkte Bilder";
-                pictures = new LinkedList<Picture>();
-                int start = 1;
-                int end = 0;
-                do {
-                  do end++; while (end < value.length() && value.charAt(end) != '-');
-                  if (start == end)
-                    return;
-                  addPicture(Long.parseLong(value.substring(start, end)));
-                  start = end + 1;
-                }
-                while (true);
-              }
-              else {
-                try {
-                  selection = (SavedSelection) selectionDao.get(Long.parseLong(value));
-                  name = selection.getName();
-                  pictures = selection.getPictures();
-                  return;
-                }
-                catch (NullPointerException e) {
-                  log.info("Die gemerkte Auswahl {} existiert nicht (mehr)!", value);
-                  break;
-                }
-              }
-            }
+    Cookie[] cookies = request.getCookies();
+    SavedSelection selection = null;
+    Cookie cookie = new Cookie(COOKIE_NAME, "");
+    cookie.setPath(COOKIE_PATH);
+    cookie.setMaxAge(COOKIE_MAX_AGE);
+    if (cookies != null) {
+      for (Cookie received : cookies) {
+        if (COOKIE_NAME.equals(received.getName())) {
+          received.setPath(COOKIE_PATH);
+          String value = received.getValue();
+          cookie = received;
+          try {
+            selection = (SavedSelection) selectionDao.get(Long.parseLong(value));
           }
+          catch (NullPointerException e) {
+            log.info("Die gemerkte Auswahl {} existiert nicht (mehr)!", value);
+          }
+          break;
         }
       }
-      catch (NumberFormatException e) {
-        log.info("Ignoriere invaliden Cookie \"{}\": {}", cookie.getValue(), e);
-      }
-
-
-      if (true) {
-        name = "Unbenannt";
-        selection = new SavedSelection();
-        selection.setName(name);
-        pictures = selection.getPictures();
-      }
-      else {
-        name = "Gemerkte Bilder";
-        pictures = new LinkedList<Picture>();
-      }
-
-      cookie = new Cookie(COOKIE_NAME,"");
-      cookie.setPath(COOKIE_PATH);
-      cookie.setMaxAge(COOKIE_MAX_AGE);
     }
 
-
-    @Override
-    public Long getId() {
-      return id;
+    if (selection == null) {
+      selection = new SavedSelection();
+      selection.setName((new Date()).toString());
+      selectionDao.save(selection);
+      cookie.setValue(selection.getId().toString());
     }
 
-    public String getName() {
-      return name;
-    }
+    selection.getPictures().add(pictureDao.get(id));
+    response.addCookie(cookie);
 
-    public void setName(String name) {
-      this.name = name;
-    }
-
-    @Override
-    public List<Picture> getPictures() {
-      return pictures;
-    }
-
-    public final void addPicture(Long id) {
-      Picture picture = pictureDao.get(id);
-      if (picture == null)
-        return;
-      if (pictures.contains(picture))
-        return;
-      pictures.add(picture);
-    }
-
-    public void removePicture(Long id) {
-      pictures.remove(pictureDao.get(id));
-    }
-
-    public Cookie toCookie() {
-      StringBuilder builder = new StringBuilder();
-      if (selection == null) {
-        Iterator<Picture> iterator = pictures.iterator();
-        while (iterator.hasNext()) {
-          builder.append('-');
-          builder.append(iterator.next().getId());
-        }
-      }
-      else {
-        selection.setName(name);
-        if (selection.getId() == null)
-          selectionDao.save(selection);
-        builder.append(selection.getId());
-      }
-      cookie.setValue(builder.toString());
-      return cookie;
-    }
-
-
-    @Override
-    public String toString() {
-      return name;
-    }
+    ModelAndView mav = new ModelAndView(VIEW);
+    mav.addObject("selection", selection);
+    return mav;
   }
 }
